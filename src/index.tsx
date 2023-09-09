cfb5f4f4333bc84cfe3bad8a45eeb510cd0f7fad
moonjiyoung <umarg228@naver.com>
2023-09-09 13:20:02 +0900

    feat: 기초 setting 변경

--- src/index.tsx
+++ src/index.tsx
@@ -59,9 +59,11 @@ table {
   box-sizing: border-box;
 }
 body {
+  font-weight: 300;
   font-family: 'Hind', sans-serif;
-  background-color: ${props => props.theme.bgColor};
   color: black;
+  line-height: 1.2;
+  background: linear-gradient(135deg, #e09, #d0e);
 }
 a {
   text-decoration: none;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#82ccdd", //blue계열
-  boardColor: "#DADFE9", //gray계열
-  cardColor: "white",
-};
+export const darkTheme: DefaultTheme = {};
