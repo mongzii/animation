e62b02dcec7143c5e552ef3a97a95c5729c2e8b9
moonjiyoung <umarg228@naver.com>
2023-09-09 13:10:43 +0900

    feat: 기초 setting

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#82ccdd", //blue계열
+  boardColor: "#DADFE9", //gray계열
+  cardColor: "white",
+};
